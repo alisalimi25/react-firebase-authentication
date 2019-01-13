f95bea72d0f5ea762647f894c83ab0cf11d05da9
Alireza Salimi <ali@Alirezas-MacBook-Pro-2.local>
2019-01-13 13:12:15 -0800

    1312

---
+++ src/components/PasswordForget/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class PasswordForget extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>Password Forget</h1>
+      </div>
+    );
+  }
+}
