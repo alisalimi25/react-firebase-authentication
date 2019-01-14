840de3dcde4deff365d1dc8d84a8cdf6730fbae7
Alireza Salimi <ali@Alirezas-MacBook-Pro-2.local>
2019-01-13 22:18:59 -0800

    2218

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,4 +1,5 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 
 var config = {
   apiKey: "AIzaSyD9dEdfBmvk0YHiViVCz6kOTR97Sc6C61U",
@@ -12,5 +13,20 @@ var config = {
 export default class Firebase {
   constructor() {
     app.initializeApp(config);
+
+    this.auth = app.auth();
   }
+
+  doCreateUserWithEmailAndPassword = (email, password) =>
+      this.auth.createUserWithEmailAndPassword(email, password);
+
+  doSignInWithEmailAndPassword = (email, password) =>
+      this.auth.signInWithEmailAndPassword(email, password);
+  
+  doSignOut = () => this.auth.signOut;
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
+
 }
